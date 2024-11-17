ac162d0b7419634c0e8428ae9ef015dc2b0f6645
Ricardo Vieira <103327245+ricaxov@users.noreply.github.com>
2024-11-17 12:38:17 -0300

    Update matching.cpp

--- matching.cpp
+++ matching.cpp
@@ -1,14 +1,30 @@
+// Prefix Function {{{
 vector<int> prefix_function(string const& S) {
   int N = size(S);
-  vector<int> P(N, 0);
+  vector<int> pi(N, 0);
   for (int i = 1; i < N; i++) {
-    int j = P[i-1];
-    while (j > 0 && S[i] != S[j]) j = P[j-1];
+    int j = pi[i-1];
+    while (j > 0 && S[i] != S[j]) j = pi[j-1];
     if (S[i] == S[j]) j++;
-    P[i] = j;
+    pi[i] = j;
   }
-  return P;
+  return pi;
 }
+//}}}
+
+// Z Function {{{
+vector<int> z_function(string const& S) {
+  int N = size(S);
+  vector<int> z(N);
+  int l = 0, r = 0;
+  for (int i = 1; i < N; i++) {
+    if (i < r) z[i] = min(r-i, z[i-l]);
+    while (i + z[i] < N && S[z[i]] == S[i+z[i]]) z[i]++;
+    if (i + z[i] > r) l = i, r = i+z[i];
+  }
+  return z;
+}
+//}}}
 
 vector<int> count_prefix(string const& S) {
   int N = size(S);
